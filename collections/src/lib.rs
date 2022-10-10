8fa9627e80b587158663ec23da4af277eebc1c59
smallnest <smallnest@gmail.com>
2022-10-10 15:52:00 +0800

    add more examples

--- collections/src/lib.rs
+++ collections/src/lib.rs
@@ -1,5 +1,5 @@
 use std::{sync::{Arc, Mutex}, collections::HashMap};
-
+use std::collections::LinkedList;
 
 pub fn common_thread_safe_collections() {
     let map: HashMap<i32, i32> = HashMap::new();
@@ -19,4 +19,45 @@ pub fn common_thread_safe_collections() {
     }
 
     println!("HashMap: {:?}", *m.lock().unwrap());
+}
+
+
+pub fn common_thread_safe_vec() {
+    let vec1 = vec![];
+    let vec2 = Arc::new(Mutex::new(vec1));
+
+    let mut handles = vec![];
+    for i in 0..10 {
+        let vec3 = Arc::clone(&vec2);
+        handles.push(std::thread::spawn(move || {
+            let mut v = vec3.lock().unwrap();
+            v.push(i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("vec: {:?}",  vec2.lock().unwrap());
+}
+
+pub fn common_thread_safe_linkedlist() {
+    let list1: LinkedList<u32> = LinkedList::new();
+    let list2 = Arc::new(Mutex::new(list1));
+
+    let mut handles = vec![];
+    for i in 0..10 {
+        let list3 = Arc::clone(&list2);
+        handles.push(std::thread::spawn(move || {
+            let mut v = list3.lock().unwrap();
+            v.push_back(i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("LinkedList: {:?}",  list2.lock().unwrap());
 }
--- collections/src/main.rs
+++ collections/src/main.rs
@@ -2,4 +2,7 @@ use collections::*;
 
 fn main() {
     common_thread_safe_collections();
+    common_thread_safe_vec();
+    common_thread_safe_linkedlist();
+    
 }

0e60452aa64be81201bc81fefe86473f546a0ca0
smallnest <smallnest@gmail.com>
2023-11-12 22:35:49 +0800

    add chapter 11

--- .history/timer_examples/src/tickers_20230212213756.rs
+++
@@ -1,9 +0,0 @@
-use ticker::Ticker;
-use std::time::Duration;
-
-pub fn ticker_example() {
-    let ticker = Ticker::new(0..10, Duration::from_secs(1));
-    for i in ticker {
-        println!("{:?}", i)
-    }
-}
